d14cc0e994138a46e04dfa29dff8fc449336eee9
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-08-29 16:26:59 +0200

    Refactor code

--- src/main.ts
+++ src/main.ts
@@ -4,55 +4,111 @@ import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 import * as os from 'os';
 import * as path from 'path';
+import { stringLiteral } from '@babel/types';
 
-export async function install(version: string) {
-    
-    const platformName = os.platform()
+function extractDataFromLsb(raw: string): string {
+    const secondPart = raw.split('\t')[1];
+    return secondPart.substr(0, secondPart.length-1);
+}
 
-    if (platformName === "linux") {
-        let myOutput = ""      
-        const options = {
-            listeners: {
-                stdout: (data: Buffer) => {
-                    myOutput += data.toString();
-                }
+async function lsbRelease(option: string): Promise<string> {
+    let execOutput = "";
+    const execOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                execOutput += data.toString();
             }
         }
-        await exec.exec('lsb_release', ['-r'], options);
-        core.warning(myOutput);
-        let uversion = myOutput.split('\t')[1];
-        uversion = uversion.substring(0, uversion.length-1);
-        core.warning(uversion);
-        const dotLessPlatformNum = uversion.replace('.', '');
-
-        let swiftPath = tc.find('swift', version);
-        let actualPath = '';
-        if (!swiftPath) {
-            try {
-                const url = 'https://swift.org/builds/swift-'+version+'-release/ubuntu'+dotLessPlatformNum+'/swift-'+version+'-RELEASE/swift-'+version+'-RELEASE-ubuntu'+uversion+'.tar.gz';
-                core.warning(url);
-                const tarPath = await tc.downloadTool(url);
-                await io.mkdirP(os.homedir() + '/swift-downloads/')
-                swiftPath = await tc.extractTar(tarPath, os.homedir() + '/swift-downloads/')
-                let subdir = 'swift-'+version+'-RELEASE-ubuntu'+ uversion
-                exec.exec('ls', [swiftPath + '/' + subdir]);
-                exec.exec('ls', [swiftPath + '/' + subdir + '/usr/bin/']);
-                exec.exec('sudo', ['apt-get', 'install', '-y', 'clang', 'libicu-dev'])
-                actualPath = swiftPath + '/' + subdir;
-                await tc.cacheDir(actualPath, 'swift', version);
-            } catch(e) {
-                core.setFailed("Couldn't download and install Swift, error: " + e);
-                return
-            }
-        }
-        
-        
-        await core.addPath(path.join(actualPath, 'usr', 'bin'));
-    } else {
-        core.setFailed("Platform " + platformName + " isn't currently supported.");
-        return
     }
-        
+    await exec.exec("lsb_release", [option], execOptions);
+    return extractDataFromLsb(execOutput);
+}
+
+interface PlatformInfo {
+    platform: string;
+    distributor: string;
+    release: string;
+}
+
+async function getPlatformInfo(): Promise<PlatformInfo> {
+    const platformName = os.platform();
+    if (platformName === "linux") {
+        let distributor: string;
+        let release: string;
+        try {
+            return {
+                platform: platformName,
+                distributor: await lsbRelease("-i"),
+                release: await lsbRelease("-r")
+            };
+        } catch(e) {
+            throw new Error("The action only support GNU Linux currently. Error: " + e)
+        }
+    } else {
+        throw new Error(platformName + "isn't currently supported");
+    }
+}
+
+function normalizeSwiftVersion(swiftVersion: string): string {
+    let nsv: string;
+    let splitVersion = swiftVersion.split('.')
+    let partCount = splitVersion.length;
+    if (partCount === 2) {
+        nsv = swiftVersion;
+    } else if (partCount === 3) {
+        if (splitVersion[2] === '0') {
+            nsv = splitVersion[0] + '.' + splitVersion[1]
+        } else {
+            nsv = swiftVersion;
+        }
+    } else {
+        throw new Error("Invalid Swift verison " + swiftVersion)
+    }
+    return nsv;
+}
+
+async function getDownloadURL(swiftVersion: string, platformInfo: PlatformInfo): Promise<string> {
+    // Normalize Swift version
+    let nsv = swiftVersion;
+
+    // Dotless release
+    let osName = platformInfo.distributor + platformInfo.release
+    let dlOSName = osName.replace('.','');
+    return 'https://swift.org/builds/swift-'+nsv+'-release/'+dlOSName+'/swift-'+nsv+'-RELEASE/swift-'+nsv+'-RELEASE-ubuntu'+osName+'.tar.gz';
+}
+
+async function downloadAndExtract(swiftURL: string, destination: string): Promise<string> {
+    let donwloadPath = await tc.downloadTool(swiftURL)
+
+    if (swiftURL.endsWith(".tar.gz")) {
+        return await tc.extractTar(donwloadPath, destination);
+    } else {
+        throw new Error('Unrecognized archive format')
+    }
+}
+
+export async function install(swiftVersion: string) {
+    const platformInfo = await getPlatformInfo();
+    const nsv = normalizeSwiftVersion(swiftVersion);  
+    const downloadURL = await getDownloadURL(swiftVersion, platformInfo);
+
+
+    // Download and extract the Swift tools
+    let toolBaseDir = process.env["RUNNER_TOOLSDIRECTORY"] || "";
+    if (toolBaseDir == "") {
+        toolBaseDir = os.homedir()  
+    }
+    const swiftDownloadsDir = path.join(toolBaseDir, '/swift-downloads/');
+    let extractedPath = await downloadAndExtract(downloadURL, swiftDownloadsDir);
+    let binPath = path.join(extractedPath, "/usr", "/bin/");
+
+    // Install required dependencies
+    if (platformInfo.platform === "linux") {
+        await exec.exec('apt-get', ['install', '-y', 'clang', 'libicu-dev']);
+    }
+
+    // Add swift tools to the path
+    await core.addPath(binPath);
 }
 
 async function run() {
