90edadf118d2225ca7f66031532b38ecfb7ceb1a
Trevör <adtrevor@users.noreply.github.com>
2019-08-29 05:55:06 +0200

    catch error in test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,9 +1,15 @@
 import * as exec from '@actions/exec';
+import * as core from '@actions/core';
 import * as main from '../src/main';
 
 describe('Test suite', () => {
   it('Attempt to call Swift', async () => {
     await main.install('5.0.2');
+    try {
     await exec.exec("swift", ["--version"]);
+    } catch(e) { 
+    core.setFailed(e)
+    return
+    }
   }, 120000);
 });
