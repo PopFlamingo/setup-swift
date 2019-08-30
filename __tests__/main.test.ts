f1a25a3b3c8df80526e2ec59682213adc9ac192e
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-08-30 18:13:49 +0200

    Add even more debug info

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,6 +7,7 @@ describe('Test suite', () => {
   it('Comming soon... tests', async () => {
     core.warning("Called test suite");
     const installPath = await main.install("5.0.2");
+    core.warning("After calling install");
     const binpath = path.join(installPath, '/usr', '/bin');
     await exec.exec(path.join(binpath, '/swift'), ['--version']);
   }, 180000);
